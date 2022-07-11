7cb5d3e82a68269c836ad0f3e674be04ddfb57b9
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-07-11 08:26:18 +0800

    nice

---
+++ src/pages/sample.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const sample = props => {
+  return (
+    <div>sample</div>
+  )
+}
+
+sample.propTypes = {}
+
+export default sample
