8ff0a43d4767811b919affee4c11af306a64a398
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-07-12 19:57:16 +0800

    update

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -1,58 +1,53 @@
-import { Box, Button, Typography } from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { Box, Typography } from "@mui/material";
 import { motion, useAnimation } from "framer-motion";
 
 const Introduction = () => {
     const controls = useAnimation();
-    const menuControls = useAnimation();
 
-    const sequence = async () => {
-        await menuControls.start({ x: "150%" });
-        return await controls.start({
-            x: "-150%",
+    const sequence = () => {
+        return controls.start({
+            y: "-150%",
         });
     };
 
     return (
         <>
             <Box
-                zIndex={1000}
+                component={motion.div}
+                animate={controls}
+                transition={{ ease: "easeOut", duration: 1 }}
+                position="fixed"
+                initial={false}
+                top={0}
                 display="flex"
                 flexDirection="column"
-                position="fixed"
-                height="100vh"
-                top={0}
-                bottom={0}
+                justifyContent="center"
+                alignItems="center"
+                width="100%"
+                height="100%"
+                sx={{ backgroundColor: "green" }}
             >
+                <Typography variant="h3" textAlign="center" color="white">
+                    Hello World, I am Vincent Dizon
+                </Typography>
                 <Box
-                    animate={menuControls}
-                    sx={{
-                        backgroundColor: "red",
-                        width: "100%",
-                        height: "100vh",
-                    }}
                     component={motion.div}
-                    position="fixed"
-                    initial={false}
-                    transition={{ ease: "easeOut", duration: 1 }}
-                >
-                    <Typography>Hi, I am</Typography>
-                </Box>
-                <Box
-                    animate={controls}
-                    sx={{
-                        backgroundColor: "green",
-                        width: "100%",
-                        height: "100vh",
-                        marginTop: 40,
+                    animate={{
+                        y: "15%",
                     }}
-                    component={motion.div}
-                    position="fixed"
-                    transition={{ ease: "easeOut", duration: 1 }}
+                    transition={{
+                        repeat: Infinity,
+                        type: "spring",
+                        bounce: 0.8,
+                        duration: 1,
+                    }}
+                    onClick={sequence}
                 >
-                    <Typography>Vincent Dizon</Typography>
-                    <Button onClick={sequence} sx={{ color: "white" }}>
-                        CLICK
-                    </Button>
+                    <ArrowDownwardIcon
+                        fontSize="large"
+                        sx={{ fontSize: 70, color: "white" }}
+                    />
                 </Box>
             </Box>
             <Box height="100vh" />
