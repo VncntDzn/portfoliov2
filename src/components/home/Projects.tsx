9daca96923031c220c2acd1b7fb33bbb15797f91
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-07-18 07:58:19 +0800

    updated project

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,5 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
 const Projects = () => {
-    return <div>Projects</div>;
+    return (
+        <Box
+            sx={{
+                background: "white",
+                paddingY: { xs: 5, lg: 10 },
+                paddingX: { xs: 5, lg: 20, xl: 50 },
+            }}
+        >
+            <Typography variant="h3">Featured Projects</Typography>
+            <Typography>Take a look on our projects.</Typography>
+        </Box>
+    );
 };
 
 Projects.propTypes = {};
