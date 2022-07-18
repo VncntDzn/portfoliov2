91ca211728356d9362d359a6b78b0cbbd6501dd5
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-07-18 09:26:41 +0800

    updated project

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,28 +1,50 @@
 import { Box, Typography } from "@mui/material";
 import Counter from "components/Counter";
 
+import { motion, useAnimation } from "framer-motion";
 const Projects = () => {
+    const controls = useAnimation();
+    const textAnimation = useAnimation();
+    const handleStartProjectShowcase = async () => {
+        await controls.start({
+            y: "-200%",
+            x: "-60%",
+            transition: { duration: 2 },
+        });
+        return await textAnimation.start({
+            x: "-28%",
+            transition: { duration: 2 },
+            textDecoration: "underline",
+        });
+    };
     return (
-        <>
-            <Box
-                sx={{
-                    background: "white",
-                    paddingY: { xs: 5, lg: 10 },
-                    paddingX: { xs: 5, lg: 20, xl: 50 },
-                    height: "100vh",
-                }}
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                position="relative"
-                zIndex={1}
-            >
-                <Typography variant="h3">Featured Projects</Typography>
-                <Typography>Take a look on our projects.</Typography>
-                <Counter count={2} />
+        <Box
+            sx={{
+                background: "white",
+                paddingY: { xs: 5, lg: 10 },
+                paddingX: { xs: 5, lg: 20, xl: 50 },
+                height: "105vh",
+            }}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            position="relative"
+            zIndex={1}
+        >
+            <Box component={motion.div} animate={controls}>
+                <Typography variant="h2">Featured Projects</Typography>
+                <Typography
+                    component={motion.p}
+                    animate={textAnimation}
+                    onClick={handleStartProjectShowcase}
+                    sx={{ cursor: "pointer", textAlign: "center" }}
+                >
+                    Take a look on our projects.
+                </Typography>
             </Box>
-        </>
+            <Counter count={2} />
+        </Box>
     );
 };
 
